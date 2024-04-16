ffa44f09331fc5a11c767a9241e59120d60b4d26
Codiadem <chiomaekpemerechi@gmail.com>
2024-04-16 14:22:27 +0100

    chore: update FindJobPage.jsx with more divs for job ad.

--- src/pages/FindJobPage.jsx
+++ src/pages/FindJobPage.jsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import "../style/find-job.css";
 import search from "../images/search-glass.png";
 
@@ -10,23 +11,67 @@ function FindJobPage() {
           <img src={search} alt="" />
         </button>
       </div>
-      <div className="job-box-container">
-        <div className="job-box">
-          <div className="job-circle"></div>
-        </div>
-        <div className="job-box">
-          <div className="job-circle"></div>
-        </div>
-        <div className="job-box">
-          <div className="job-circle"></div>
-        </div>
-        <div className="job-box">
-          <div className="job-circle"></div>
-        </div>
-        <div className="job-box">
-          <div className="job-circle"></div>
+      <div className="array">
+        <h3>Latest Remote Jobs</h3>
+        <div className="job-box-container">
+          <div className="job-box">
+            <div className="job-circle"></div>
+          </div>
+          <div className="job-box">
+            <div className="job-circle"></div>
+          </div>
+          <div className="job-box">
+            <div className="job-circle"></div>
+          </div>
+          <div className="job-box">
+            <div className="job-circle"></div>
+          </div>
+          <div className="job-box">
+            <div className="job-circle"></div>
+          </div>
         </div>
       </div>
+      <div className="array">
+        <h3>Latest Fully remote Jobs</h3>
+        <div className="job-box-container">
+          <div className="job-box">
+            <div className="job-circle"></div>
+          </div>
+          <div className="job-box">
+            <div className="job-circle"></div>
+          </div>
+          <div className="job-box">
+            <div className="job-circle"></div>
+          </div>
+          <div className="job-box">
+            <div className="job-circle"></div>
+          </div>
+          <div className="job-box">
+            <div className="job-circle"></div>
+          </div>
+        </div>
+      </div>
+      <div className="array">
+        <h3>Latest Onsite Jobs</h3>
+        <div className="job-box-container">
+          <div className="job-box">
+            <div className="job-circle"></div>
+          </div>
+          <div className="job-box">
+            <div className="job-circle"></div>
+          </div>
+          <div className="job-box">
+            <div className="job-circle"></div>
+          </div>
+          <div className="job-box">
+            <div className="job-circle"></div>
+          </div>
+          <div className="job-box">
+            <div className="job-circle"></div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 }
